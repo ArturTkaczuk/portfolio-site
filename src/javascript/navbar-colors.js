c642c6f22debcbdbe4b4b185d1a511b2af3c9eac
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-06 10:58:34 +0200

    Add navbar highlight on scroll - navbar-colors.js

---
+++ src/javascript/navbar-colors.js
@@ -0,0 +1,25 @@
+const sections = [
+  document.querySelector("#about"),
+  document.querySelector("#skills"),
+  document.querySelector("#projects"),
+  document.querySelector("#contact"),
+];
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 100;
+    const sectionId = current.getAttribute("id");
+    console.log(sectionId);
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('li a[href*="' + sectionId + '"]')
+        .classList.add("active");
+    } else {
+      document
+        .querySelector('li a[href*="' + sectionId + '"]')
+        .classList.remove("active");
+    }
+  });
+});
